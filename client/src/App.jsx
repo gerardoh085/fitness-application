0a60e3f9136509383401098942f6b83b8474e706
gerardoh085 <gerardoh085@gmail.com>
2023-10-16 17:18:52 -0400

    server and client connection adjustments, navbar style updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,24 +7,35 @@ import Papa from "papaparse";
 import CSVReader from "react-csv-reader";
 import axios from "axios";
 
-import Navbar from "./components/Navbar";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  const [data, setData] = useState([]);
-  const API_KEY = "dfe5654b3738db5031f5178cfeaf4826e99ef713";
-  const header = {
-    method: "GET",
-    limit: "10",
-    offset: "10",
-  };
-  const fUrl = "https://wger.de/api/v2/exerciseimage/?limit=20&offset=20";
+  // const [data, setData] = useState([]);
+  // const API_KEY = "dfe5654b3738db5031f5178cfeaf4826e99ef713";
+  // const header = {
+  //   method: "GET",
+  //   limit: "10",
+  //   offset: "10",
+  // };
+  // const fUrl = "https://wger.de/api/v2/exerciseimage/?limit=20&offset=20";
+  // useEffect(() => {
+  //   fetch(fUrl, header)
+  //     .then((res) => res.json())
+  //     .then((resdata) => setData(resdata.results));
+  // }, []);
+
+  const [backend, setBackend] = useState([]);
+
   useEffect(() => {
-    fetch(fUrl, header)
+    fetch("http://localhost:8080/")
       .then((res) => res.json())
-      .then((resdata) => setData(resdata.results));
+      .then((resdata) => {
+        setBackend(resdata.tests);
+      })
+      .catch((err) => console.log(err));
   }, []);
 
-  console.log(data);
+  console.log(backend);
   return (
     <>
       <div className="container">
--- server/app.js
+++ server/app.js
@@ -2,22 +2,23 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 // middleware
 const morgan = require("morgan");
 
 const app = express();
 const port = 8080;
 
-const dbURI = "";
+// const dbURI = "";
 
-mongoose
-  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then((result) => {
-    console.log("connected to db");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+// mongoose
+//   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then((result) => {
+//     console.log("connected to db");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 // register view engine:
 // app.set('view engine', 'ejs');
@@ -32,6 +33,20 @@ app.listen(port, () => {
 
 // middleware setup:
 app.use(morgan("dev"));
+
 // url encoded data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({ origin: "http://localhost:5173" }));
+
+app.get("/", (req, res) => {
+  console.log("get req home page");
+  res.json({ tests: ["testing 1", "testing 2"] });
+});
+app.get("/exercises", (req, res) => {});
+app.get("/exercises/:id", (req, res) => {});
+app.get("/user/progress/:id", (req, res) => {});
+app.post("/user/progress/:id", (req, res) => {});
+app.post("/user", (req, res) => {});
+app.get("/nutrition", (req, res) => {});
